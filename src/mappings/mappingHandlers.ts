e1d51f01f5fab5ad48cb19e2b5fd296eb1760e66
Sotatek-TanHoang <tan.hoang@sotatek.com>
2022-07-07 10:17:21 +0700

    working version of dictionary coden is not work right now schema.graphql is still in progress

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,16 +1,34 @@
-import {Transactions} from "../types";
-
+import { Transactions } from "../types";
 
 export async function handleTransaction(txn: any): Promise<void> {
-  const newTransaction= new Transactions(`${txn.id}`);
+  const newTransaction = new Transactions(`${txn.id}`);
 
-  newTransaction.txType=txn.txType;
-  
-    
+  newTransaction.txType = txn.txType;
 
-
+  if (newTransaction.txType === "acfg") {
+    newTransaction.assetId = BigInt(txn?.assetConfigTransaction?.assetId);
+  }
+  if (newTransaction.txType === "afrz") {
+    newTransaction.newFreezeStatus = txn.assetFreezeTransaction.newFreezeStatus;
+    newTransaction.address = txn.assetFreezeTransaction.address;
+    newTransaction.assetId = txn.assetFreezeTransaction.assetId;
+  }
+  if (newTransaction.txType === "appl") {
+    newTransaction.applicationId = BigInt(
+      txn.applicationTransaction.applicationId
+    );
+  }
+  if (newTransaction.txType === "axfer") {
+    newTransaction.sender = txn.sender;
+    newTransaction.receiver = txn.assetTransferTransaction.receiver;
+  }
+  if (newTransaction.txType === "keyreg") {
+    newTransaction.nonParticipant = txn.keyregTransaction.nonParticipation;
+  }
+  if (newTransaction.txType === "pay") {
+    newTransaction.sender = txn?.sender;
+    newTransaction.receiver = txn?.paymentTransaction.receiver;
+  }
 
   await newTransaction.save();
 }
-
-
