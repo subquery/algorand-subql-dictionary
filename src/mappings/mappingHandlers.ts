576d3abe75c949abc70f726923efd5b45a2c5bf5
Scott Twiname <skott.twiname@gmail.com>
2022-08-04 09:29:16 +1200

    Add assetId to axfer tx, clean up

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,36 +1,34 @@
+import { TransactionType } from 'algosdk';
 import { AlgorandTransaction, AlgorandBlock } from "@subql/types-algorand";
 import { Transaction } from "../types";
 
-function handleTransaction(txn: AlgorandTransaction): Transaction {
-  const newTransaction = new Transaction(`${txn.id}`);
+function handleTransaction(tx: AlgorandTransaction): Transaction {
+  const transaction = new Transaction(`${tx.id}`);
 
-  newTransaction.txType = txn.txType;
-  newTransaction.blockHeight = BigInt(txn.confirmedRound);
-  if (newTransaction.txType === "acfg") {
-    newTransaction.assetId = BigInt(txn?.assetConfigTransaction?.assetId);
-  }
-  if (newTransaction.txType === "afrz") {
-    newTransaction.newFreezeStatus = txn.assetFreezeTransaction.newFreezeStatus;
-    newTransaction.address = txn.assetFreezeTransaction.address;
-    newTransaction.assetId = BigInt(txn.assetFreezeTransaction.assetId);
-  }
-  if (newTransaction.txType === "appl") {
-    newTransaction.applicationId = 
-    BigInt(txn.applicationTransaction.applicationId);
-  }
-  if (newTransaction.txType === "axfer") {
-    newTransaction.sender = txn.sender;
-    newTransaction.receiver = txn.assetTransferTransaction.receiver;
-  }
-  if (newTransaction.txType === "keyreg") {
-    newTransaction.nonParticipant = txn.keyregTransaction.nonParticipation;
-  }
-  if (newTransaction.txType === "pay") {
-    newTransaction.sender = txn?.sender;
-    newTransaction.receiver = txn?.paymentTransaction.receiver;
+  transaction.txType = tx.txType;
+  transaction.blockHeight = BigInt(tx.confirmedRound);
+
+  switch(tx.txType) {
+    case TransactionType.acfg:
+      transaction.assetId = BigInt(tx.assetConfigTransaction.assetId);
+    case TransactionType.afrz:
+      transaction.newFreezeStatus = tx.assetFreezeTransaction.newFreezeStatus;
+      transaction.address = tx.assetFreezeTransaction.address;
+      transaction.assetId = BigInt(tx.assetFreezeTransaction.assetId);
+    case TransactionType.axfer:
+      transaction.sender = tx.sender;
+      transaction.receiver = tx.assetTransferTransaction.receiver;
+      transaction.assetId = BigInt(tx.assetTransferTransaction.assetId);
+    case TransactionType.appl:
+      transaction.applicationId = BigInt(tx.applicationTransaction.applicationId);
+    case TransactionType.keyreg:
+      transaction.nonParticipant = tx.keyregTransaction.nonParticipation;
+    case TransactionType.pay:
+      transaction.sender = tx.sender;
+      transaction.receiver = tx.paymentTransaction.receiver;
   }
 
-  return newTransaction;
+  return transaction;
 }
 
 export async function handleBlock(block: AlgorandBlock): Promise<void> {
